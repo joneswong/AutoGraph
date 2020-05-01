ac44c49c8ac904c2b72f50aaf92557666ddd7424
yuexiang.xyx <yuexiang.xyx@alibaba-inc.com>
2020-05-02 00:18:38 +0800

    feature engineer: remove uninformative features, pca, add new feature (node degree)
    Link: https://code.aone.alibaba-inc.com/jones.wz/KDD20AutoGraph/codereview/2909262
    
    * feature engineer: remove uninformative features, pca, add new feature (node degree)
    
    * feature engineer: remove uninformative features, pca, add new feature (node degree)
    
    * feature engineer: remove uninformative features, pca, add new feature (node degree)
    
    * feature engineer: remove uninformative features, pca, add new feature (node degree)

--- code_submission/utils.py
+++ code_submission/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import torch
 from torch_geometric.data import Data
+from feature_engineer import dim_reduction, feature_generation
 
 
 def fix_seed(seed):
@@ -17,27 +18,20 @@ def fix_seed(seed):
     torch.backends.cudnn.deterministic = True
 
 
-def generate_pyg_data(data):
+def generate_pyg_data(data, use_dim_reduction=True, use_feature_generation=True, sparse_threshold=0.9, pca_threshold=0.75):
 
     x = data['fea_table']
-    if x.shape[1] == 1:
-        x = x.to_numpy()
-        x = x.reshape(x.shape[0])
-        x = np.array(pd.get_dummies(x))
-    else:
-        x = x.drop('node_index', axis=1).to_numpy()
-
-    x = torch.tensor(x, dtype=torch.float)
 
     df = data['edge_file']
     edge_index = df[['src_idx', 'dst_idx']].to_numpy()
     edge_index = sorted(edge_index, key=lambda d: d[0])
     edge_index = torch.tensor(edge_index, dtype=torch.long).transpose(0, 1)
 
+
     edge_weight = df['edge_weight'].to_numpy()
     edge_weight = torch.tensor(edge_weight, dtype=torch.float32)
 
-    num_nodes = x.size(0)
+    num_nodes = x.shape[0]
     y = torch.zeros(num_nodes, dtype=torch.long)
     inds = data['train_label'][['node_index']].to_numpy()
     train_y = data['train_label'][['label']].to_numpy()
@@ -45,6 +39,16 @@ def generate_pyg_data(data):
 
     train_indices = data['train_indices']
     test_indices = data['test_indices']
+
+    ###   feature engineering  ###
+    if use_dim_reduction:
+        x = dim_reduction(x, sparse_threshold, pca_threshold)
+
+    if x.shape[1] == 1 and use_feature_generation:
+        added_feature = feature_generation(x, edge_index)
+        x = np.concatenate([x, added_feature], axis=1)
+    
+    x = torch.tensor(x, dtype=torch.float)
 
     data = Data(x=x, edge_index=edge_index, y=y, edge_weight=edge_weight)
 
