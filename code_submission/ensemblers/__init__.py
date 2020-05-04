38332314eef3f6e47f92a2e790aa6534ecf7564a
jones.wz <jones.wz@alibaba-inc.com>
2020-05-04 20:44:01 +0800

    cross validation
    enabled cross validation, move cuda init to the constructor, fix the data preparation issue
    
    Link: https://code.aone.alibaba-inc.com/jones.wz/KDD20AutoGraph/codereview/2910427
    
    * enabled convenient benchmarking
    
    * move cuda initialization to constructor
    
    * fixed data preparation issue

--- code_submission/ensemblers/__init__.py
+++ code_submission/ensemblers/__init__.py
@@ -3,11 +3,9 @@ from __future__ import division
 from __future__ import print_function
 
 from ensemblers.ensembler import Ensembler
-from ensemblers.greedy_strategy import GreedyStrategy
 
 
 agents = dict(
-    Ensembler=Ensembler,
-    GreedyStrategy=GreedyStrategy)
+    Ensembler=Ensembler)
 
-__all__ = ["Ensembler", "GreedyStrategy"]
+__all__ = ["Ensembler"]
