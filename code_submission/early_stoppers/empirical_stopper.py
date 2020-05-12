e8144560857f496910bb1da4db1e3ac17a39c03f
hankel.chk <hankel.chk@alibaba-inc.com>
2020-05-12 13:29:57 +0800

    default settings for early stoppers

--- code_submission/early_stoppers/empirical_stopper.py
+++ code_submission/early_stoppers/empirical_stopper.py
@@ -16,7 +16,7 @@ WINDOW_SIZE = 20
 
 class EmpiricalStopper(Stopper):
 
-    def __init__(self, min_step=40, max_step=800):
+    def __init__(self, min_step=30, max_step=500):
         self._min_step = max(min_step, WINDOW_SIZE)
         self._max_step = max_step
         self.performance_windows = [None for i in range(WINDOW_SIZE)]
