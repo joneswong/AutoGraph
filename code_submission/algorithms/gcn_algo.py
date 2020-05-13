86e1473b5861b32c72c8ac1191feb7014128f6fa
hankel.chk <hankel.chk@alibaba-inc.com>
2020-05-13 10:28:04 +0800

    added detach for focal loss

--- code_submission/algorithms/gcn_algo.py
+++ code_submission/algorithms/gcn_algo.py
@@ -94,7 +94,8 @@ class FocalLoss(torch.nn.Module):
         pt = F.softmax(input)
 
         ce_loss = torch.nn.CrossEntropyLoss(reduction="none")(input, target)
-        loss_weight = torch.sum(one_hot_target * torch.pow(1 - pt, self.gamma) * self.alpha, dim=1)
+        # loss_weight = torch.sum(one_hot_target * torch.pow(1 - pt, self.gamma) * self.alpha, dim=1)
+        loss_weight = torch.sum(one_hot_target * (torch.pow(1 - pt, self.gamma)).detach() * self.alpha, dim=1)
         loss = loss_weight * ce_loss
 
         if self.reduction == 'none':
@@ -202,15 +203,17 @@ class GNNAlgo(object):
 class GCNAlgo(GNNAlgo):
 
     hyperparam_space = dict(
-        num_layers=Categoric(list(range(2, 5)), None, 2),
-        hidden=Categoric([16, 32, 64, 128], None, 16),
+        num_layers=Categoric(list(range(2, 5)), None, 3),
+        hidden=Categoric([16, 32, 64, 128], None, 32),
         hidden_droprate=Categoric([0.3, 0.4, 0.5, 0.6], None, 0.5),
         lr=Categoric([5e-4, 1e-3, 2e-3, 5e-3, 1e-2], None, 5e-3),
         weight_decay=Categoric([0., 1e-5, 5e-4, 1e-2], None, 5e-4),
-        edge_droprate=Categoric([0., 0.2, 0.4, 0.5, 0.6], None, 0.0),
-        feature_norm=Categoric(["no_norm", "graph_size_norm"], None, "no_norm"),
+        # edge_droprate=Categoric([0., 0.2, 0.4, 0.5, 0.6], None, 0.0),
+        edge_droprate=Categoric([0.], None, 0.0),
+        # feature_norm=Categoric(["no_norm", "graph_size_norm"], None, "no_norm"),
         # todo (daoyuan): add pair_norm and batch_norm
-        loss_type=Categoric(["focal_loss", "ce_loss"], None, "focal_loss"),
+        # loss_type=Categoric(["focal_loss", "ce_loss"], None, "ce_loss"),
+        loss_type=Categoric(["ce_loss"], None, "ce_loss"),
     )
 
     def __init__(self,
@@ -232,4 +235,3 @@ class GCNAlgo(GNNAlgo):
         self._features_num = features_num
         self.loss_type = config.get("loss_type", "focal_loss")
         self.fl_loss = FocalLoss(config.get("gamma", 2), non_hpo_config.get("label_alpha", []), device)
-
