e0dac3f1432ba29855d12ead7a1c1dba672b2868
道辕 <daoyuanchen.cdy@alibaba-inc.com>
2020-05-22 00:25:57 +0800

    [WIP] DGL-Adaptive-GCN

--- code_submission/algorithms/gnns.py
+++ code_submission/algorithms/gnns.py
@@ -14,8 +14,107 @@ import torch.nn as nn
 
 from torch_geometric.utils.dropout import dropout_adj
 
-# # There are some issues in AdaGCN, I will push AdaGCN in the next version
-# class AdaGCNConv(GCNConv):
+
+def adaptive_message_func(edges):
+    """
+    send data for computing metrics and update.
+    """
+    return {'feat': edges.src['h'], 'logits': edges.src['logits']}
+
+
+def adaptive_attn_message_func(edges):
+    return {'feat': edges.src['ft'] * edges.data['a'], 'logits': edges.src['logits'], 'a': edges.data['a']}
+
+
+def adaptive_attn_reduce_func(nodes):
+    # (n_nodes, n_edges, n_classes)
+    _, pred = torch.max(nodes.mailbox['logits'], dim=2)
+    _, center_pred = torch.max(nodes.data['logits'], dim=1)
+    n_degree = nodes.data['degree']
+    # case 1
+    # ratio of common predictions
+    a = nodes.mailbox['a'].squeeze(3)  # (n_node, n_neighbor, n_head, 1)
+    n_head = a.size(2)
+    idxs = torch.eq(pred, center_pred.unsqueeze(1)).unsqueeze(2).expand_as(a)
+    f1 = torch.div(torch.sum(a * idxs, dim=1), n_degree.unsqueeze(1))  # (n_node, n_head)
+    f1 = f1.detach()
+    # case 2
+    # entropy of neighborhood predictions
+    uniq = torch.unique(pred)
+    # (n_unique)
+    cnts_p = torch.zeros((pred.size(0), n_head, uniq.size(0),)).cuda()
+    for i, val in enumerate(uniq):
+        idxs = torch.eq(pred, val).unsqueeze(2).expand_as(a)
+        tmp = torch.div(torch.sum(a * idxs, dim=1), n_degree.unsqueeze(1))  # (n_nodes, n_head)
+        cnts_p[:, :, i] = tmp
+    cnts_p = torch.clamp(cnts_p, min=1e-5)
+    f2 = (-1) * torch.sum(cnts_p * torch.log(cnts_p), dim=2)
+    f2 = f2.detach()
+    neighbor_agg = torch.sum(nodes.mailbox['feat'], dim=1)  # (n_node, n_head, n_feat)
+    return {
+        'f1': f1,
+        'f2': f2,
+        'agg': neighbor_agg,
+    }
+
+
+class GatedLayer(nn.Module):
+    def __init__(self, g, in_feats, out_feats, activation, dropout, lidx=1):
+        super(GatedLayer, self).__init__()
+        self.weight_neighbors = nn.Linear(in_feats, out_feats)
+        self.activation = activation
+        self.dropout = nn.Dropout(p=dropout)
+        self.tau_1 = nn.Parameter(torch.zeros((1,)))
+        self.tau_2 = nn.Parameter(torch.zeros((1,)))
+
+        self.ln_1 = nn.LayerNorm((g.number_of_nodes()), elementwise_affine=False)
+        self.ln_2 = nn.LayerNorm((g.number_of_nodes()), elementwise_affine=False)
+
+        self.reset_parameters(lidx)
+
+    def reset_parameters(self, lidx, how='layerwise'):
+        # initialize params
+        if how == 'normal':
+            nn.init.normal_(self.tau_1)
+            nn.init.normal_(self.tau_2)
+        else:
+            nn.init.constant_(self.tau_1, 1 / (lidx + 1))
+            nn.init.constant_(self.tau_2, 1 / (lidx + 1))
+        return
+
+    def forward(self, g, h, logits, old_z, shared_tau=True, tau_1=None, tau_2=None):
+        # TODO: training mode different from test mode
+        # operates on a node
+        g = g.local_var()
+        if self.dropout:
+            h = self.dropout(h)
+        # g.ndata['h'] = h * g.ndata['norm'] # normalization by src
+        g.ndata['h'] = h
+        g.ndata['logits'] = logits
+
+        g.update_all(message_func=fn.copy_u('logits', 'logits'), reduce_func=adaptive_reduce_func)
+        f1 = g.ndata.pop('f1')
+        f2 = g.ndata.pop('f2')
+        norm_f1 = self.ln_1(f1)
+        norm_f2 = self.ln_2(f2)
+        if shared_tau:
+            z = F.sigmoid((-1) * (norm_f1 - tau_1)) * F.sigmoid((-1) * (norm_f2 - tau_2))
+        else:
+            # tau for each layer
+            z = F.sigmoid((-1) * (norm_f1 - self.tau_1)) * F.sigmoid((-1) * (norm_f2 - self.tau_2))
+
+        gate = torch.min(old_z, z)
+        g.update_all(message_func=fn.copy_u('h', 'feat'), reduce_func=fn.sum(msg='feat', out='agg'))
+
+        agg = g.ndata.pop('agg')
+
+        # normagg = self.weight_neighbors(agg)*g.ndata['norm']
+        normagg = agg * g.ndata['norm']  # normalization by tgt degree
+
+        if self.activation:
+            normagg = self.activation(normagg)
+        new_h = h + gate.unsqueeze(1) * normagg
+        return new_h, z
 
 
 class DirectedGCNConv(GCNConv):
@@ -29,7 +128,7 @@ class DirectedGCNConv(GCNConv):
     def norm(edge_index, num_nodes, edge_weight=None, improved=False,
              dtype=None):
         if edge_weight is None:
-            edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
+            edge_weight = torch.ones((edge_index.size(1),), dtype=dtype,
                                      device=edge_index.device)
         fill_value = 1 if not improved else 2
         edge_index, edge_weight = add_remaining_self_loops(
@@ -42,7 +141,6 @@ class DirectedGCNConv(GCNConv):
                deg_inv_sqrt[col] * edge_weight * deg_inv_sqrt[row]
 
     def forward(self, x, edge_index, edge_weight=None):
-        """"""
         x = torch.matmul(x, self.weight)
         if self.cached and self.cached_result is not None:
             if edge_index.size(1) != self.cached_num_edges:
@@ -54,7 +152,7 @@ class DirectedGCNConv(GCNConv):
         if not self.cached or self.cached_result is None:
             self.cached_num_edges = edge_index.size(1)
             edge_index, norm_in, norm_out = self.norm(edge_index, x.size(0), edge_weight,
-                                         self.improved, x.dtype)
+                                                      self.improved, x.dtype)
             self.cached_result = edge_index, norm_in, norm_out
         edge_index, norm_in, norm_out = self.cached_result
         return self.propagate(edge_index, x=x, norm_in=norm_in, norm_out=norm_out)
@@ -100,7 +198,7 @@ class SplineGCN(torch.nn.Module):
             # todo (daoyuan) add layer_norm
             x = x if self.fea_norm_layer is None else self.fea_norm_layer(x)
             x = F.dropout(x, p=self.droprate, training=self.training)
-            if i == len(self.convs)-1:
+            if i == len(self.convs) - 1:
                 x = conv(x, edge_index, edge_weight)
             else:
                 x = F.elu(conv(x, edge_index, edge_weight))
